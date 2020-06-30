8e86079f8d14b62653264c9881cde8688d561434
이찬호 <chanholee@superbcorp.com>
2020-06-30 18:59:14 +0900

    개발 환경 설정

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Home() {
+  return <Title>My page</Title>;
+}
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`;
