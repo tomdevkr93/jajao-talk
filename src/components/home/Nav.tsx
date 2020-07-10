5679bd351cdf8ca3ce513447cb4bbd9c589ed63e
이찬호 <devho813@gmail.com>
2020-07-10 17:03:25 +0900

    빌드 오류 수정

--- src/components/home/Nav.tsx
+++ src/components/home/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { FaTimes } from 'react-icons/fa';
 import media from '../../utils/media-query';
