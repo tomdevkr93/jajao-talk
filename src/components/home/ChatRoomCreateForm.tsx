a583735177e7cc671141a68dbad72b0495537292
이찬호 <devho813@gmail.com>
2020-07-15 15:52:46 +0900

    채팅방 생성 모달창 리팩토링

--- src/components/home/ChatRoomCreateForm.tsx
+++ src/components/home/ChatRoomCreateForm.tsx
@@ -8,96 +8,91 @@ function ChatRoomCreateForm() {
     onSubmit,
     onChangeTitle,
     title,
-    onChangeHeadCountRange,
-    headCount,
     onClickCategoryItem,
     activeCategoryIndex,
+    onChangeHeadCountRange,
+    headCount,
   } = useChatRoomCreateForm();
 
   return (
-    <FormContainer>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="title">채팅방 제목</label>
-        <input id="title" type="text" required={true} onChange={onChangeTitle} value={title} autoComplete="off" />
-        <label>채팅방 카테고리</label>
-        <CategoryList>
-          <li className={activeCategoryIndex === 0 ? 'active' : ''} onClick={onClickCategoryItem(0)}>
-            #자유
-          </li>
-          <li className={activeCategoryIndex === 1 ? 'active' : ''} onClick={onClickCategoryItem(1)}>
-            #고민
-          </li>
-          <li className={activeCategoryIndex === 2 ? 'active' : ''} onClick={onClickCategoryItem(2)}>
-            #운동
-          </li>
-          <li className={activeCategoryIndex === 3 ? 'active' : ''} onClick={onClickCategoryItem(3)}>
-            #태그4
-          </li>
-          <li className={activeCategoryIndex === 4 ? 'active' : ''} onClick={onClickCategoryItem(4)}>
-            #태그5
-          </li>
-        </CategoryList>
-        <label htmlFor="headCount">
-          채팅방 인원수&nbsp;&nbsp;<HeadCount>{headCount}명</HeadCount>
-        </label>
-        <input
-          id="headCount"
-          type="range"
-          required={true}
-          min={1}
-          max={5}
-          step={1}
-          onChange={onChangeHeadCountRange}
-          value={headCount}
-        />
-        <button>채팅방 생성</button>
-      </form>
-    </FormContainer>
+    <Form onSubmit={onSubmit}>
+      <label htmlFor="title">채팅방 제목</label>
+      <input id="title" type="text" required={true} onChange={onChangeTitle} value={title} autoComplete="off" />
+      <label>채팅방 카테고리</label>
+      <CategoryList>
+        <li className={activeCategoryIndex === 0 ? 'active' : ''} onClick={onClickCategoryItem(0)}>
+          #자유
+        </li>
+        <li className={activeCategoryIndex === 1 ? 'active' : ''} onClick={onClickCategoryItem(1)}>
+          #고민
+        </li>
+        <li className={activeCategoryIndex === 2 ? 'active' : ''} onClick={onClickCategoryItem(2)}>
+          #운동
+        </li>
+        <li className={activeCategoryIndex === 3 ? 'active' : ''} onClick={onClickCategoryItem(3)}>
+          #태그4
+        </li>
+        <li className={activeCategoryIndex === 4 ? 'active' : ''} onClick={onClickCategoryItem(4)}>
+          #태그5
+        </li>
+      </CategoryList>
+      <label htmlFor="headCount">
+        채팅방 인원수&nbsp;&nbsp;<HeadCount>{headCount}명</HeadCount>
+      </label>
+      <input
+        id="headCount"
+        type="range"
+        required={true}
+        min={1}
+        max={5}
+        step={1}
+        onChange={onChangeHeadCountRange}
+        value={headCount}
+      />
+      <button>채팅방 생성</button>
+    </Form>
   );
 }
 
 export default ChatRoomCreateForm;
 
-const FormContainer = styled.div`
+const Form = styled.form`
   width: 100%;
   height: 100%;
   padding: 100px 50px 50px;
+  color: white;
 
-  & > form {
-    color: white;
+  label {
+    display: inline-block;
+    margin-top: 30px;
+    font-size: 16px;
+    cursor: pointer;
+  }
 
-    label {
-      display: inline-block;
-      margin-top: 30px;
-      font-size: 16px;
-      cursor: pointer;
-    }
+  input {
+    display: block;
+    margin-top: 8px;
+    padding: 8px;
+    width: 100%;
+    border-radius: 5px;
+    font-size: 14px;
+  }
 
-    input {
-      display: block;
-      margin-top: 8px;
-      padding: 8px;
-      width: 100%;
-      border-radius: 5px;
-      font-size: 14px;
-    }
+  input[type='range'] {
+    cursor: pointer;
+  }
 
-    input[type='range'] {
-      cursor: pointer;
-    }
-
-    button {
-      margin-top: 40px;
-      padding: 0.8em;
-      width: 100%;
-      border: none;
-      border-radius: 5px;
-      font-weight: 500;
-      text-align: center;
-      color: ${MAIN_COLOR};
-      cursor: pointer;
-      background-color: white;
-    }
+  button {
+    margin-top: 40px;
+    padding: 0.8em;
+    width: 100%;
+    border: none;
+    border-radius: 5px;
+    font-weight: 500;
+    text-align: center;
+    color: ${MAIN_COLOR};
+    cursor: pointer;
+    background-color: white;
   }
 `;
 
